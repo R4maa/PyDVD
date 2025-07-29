b71f05cbd6d11465fe68a0190ae414a8d03dc128
R4maa <amarrazzaq1@gmail.com>
2025-07-29 14:38:42 -0400

    comments added

--- PyDVD.py
+++ PyDVD.py
@@ -1,19 +1,20 @@
 import sys
-
-from PyQt5.QtCore import Qt, QSize
+import random
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
 
+# init main application 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
+window = QMainWindow()  # widget of main service
 
 window.setAttribute(Qt.WA_TranslucentBackground, True)
 window.setAttribute(Qt.WA_NoSystemBackground, True)
 window.setWindowFlags(Qt.FramelessWindowHint)
 
 label = QLabel(window)
-pixmap = QPixmap('logo.png').scaled(300,300, aspectRatioMode=Qt.KeepAspectRatio)
+pixmap = QPixmap('logo.png').scaled(300,300, aspectRatioMode=Qt.KeepAspectRatio) # scales image to 300 px while keeping aspect ratio
 label.setPixmap(pixmap)
 label.setGeometry(0, 0, pixmap.width(), pixmap.height())
 
@@ -22,4 +23,4 @@ window.label = label
 window.resize(pixmap.width(),pixmap.height())
 
 window.show()
-sys.exit(app.exec_())
+app.exec()
