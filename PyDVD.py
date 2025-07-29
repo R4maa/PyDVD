6cabb8d51ce53b3395f977b782e0d9589eca340c
R4maa <amarrazzaq1@gmail.com>
2025-07-29 14:56:09 -0400

    Update PyDVD.py

--- PyDVD.py
+++ PyDVD.py
@@ -1,8 +1,34 @@
 import sys
 import random
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+
+class MainWindow(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setAttribute(Qt.WA_NoSystemBackground, True)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        self.label = QLabel(self)
+        pixmap = QPixmap('logo.png').scaled(
+            300, 300, 
+            aspectRatioMode=Qt.KeepAspectRatio
+        )
+        self.label.setPixmap(pixmap)
+        self.label.setGeometry(0, 0, pixmap.width(), pixmap.height())
+
+        self.resize(pixmap.width(), pixmap.height())
+
+    def move(self):
+        screen = QApplication.desktop().availableGeometry()
+        startdir = random.randrange(1, 360)
+
+        x, y = self.x, self.y
+
 
 # init main application 
 app = QApplication(sys.argv)
